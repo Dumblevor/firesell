6380f3ac92f9fe04da866963fc4bea1e5418e16a
DMTA <100156884+Dumblevor@users.noreply.github.com>
2022-08-13 14:57:35 +0300

    1

--- config/envir.py
+++ config/envir.py
@@ -2,4 +2,3 @@ import os
 
 db_URI = os.getenv('DATABASE_URL', 'postgresql://localhost:5432/postgresql-silhouetted-08098')
 secret = os.getenv('SECRET', 'fanatstic_apps_and_where_to_find_them')
-tou
